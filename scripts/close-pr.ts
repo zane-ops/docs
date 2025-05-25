8aef8594b9c427729922b17b6eebc57f16bcc773
Fred KISSIE <fredkiss3@gmail.com>
2025-05-25 20:49:10 +0200

    docs for v1.10 (#14)

---
+++ scripts/close-pr.ts
@@ -0,0 +1,71 @@
+import {
+  DASHBOARD_URL,
+  ENV_NAME,
+  PROJECT_SLUG,
+  authenticate,
+  colors,
+  env,
+  extraHeaders,
+  parseResponseBody
+} from "./common";
+const { requestCookie, csrfToken } = await authenticate();
+
+/*****************************/
+/*     CLOSING PR ENV      */
+/*****************************/
+
+console.log(
+  `Creating new environment ${colors.blue(
+    `pr-${env.PR_NUMBER}`
+  )} in the project ${colors.blue(PROJECT_SLUG)}...`
+);
+const getEnvRequest = await fetch(
+  `${DASHBOARD_URL}/api/projects/${PROJECT_SLUG}/environment-details/${ENV_NAME}/`,
+  {
+    method: "GET",
+    headers: {
+      "x-csrftoken": csrfToken,
+      cookie: requestCookie,
+      ...extraHeaders
+    }
+  }
+);
+if (![200, 404].includes(getEnvRequest.status)) {
+  console.error(colors.red("❌ Failed to GET the environment for the PR ❌"));
+  console.error(
+    `Received status code from zaneops API : ${colors.red(getEnvRequest.status)}`
+  );
+
+  console.error("Received response from zaneops API : ");
+  console.dir(await parseResponseBody(getEnvRequest), { depth: null });
+  process.exit(1);
+}
+
+if (getEnvRequest.status === 200) {
+  const deleteEnvRequest = await fetch(
+    `${DASHBOARD_URL}/api/projects/${PROJECT_SLUG}/environment-details/${ENV_NAME}/`,
+    {
+      method: "DELETE",
+      headers: {
+        "x-csrftoken": csrfToken,
+        cookie: requestCookie,
+        ...extraHeaders
+      }
+    }
+  );
+
+  if (deleteEnvRequest.status !== 204) {
+    console.error(
+      colors.red("❌ Failed to archive the environment for the PR ❌")
+    );
+    console.error(
+      `Received status code from zaneops API : ${colors.red(getEnvRequest.status)}`
+    );
+
+    console.error("Received response from zaneops API : ");
+    console.dir(await parseResponseBody(getEnvRequest), { depth: null });
+    process.exit(1);
+  }
+}
+
+console.log(`Succesfully archived environment ${colors.blue(ENV_NAME)} ✅`);
